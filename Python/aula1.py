2d79bf2360041a475d62e4f5d5bcc504ce387179
Samuel Luciano <samuel.adm0017@gmail.com>
2025-10-19 15:58:18 -0100

    Create aula1.py

---
+++ Python/aula1.py
@@ -0,0 +1,11 @@
+#Aula 1 - Primeiros passos com Python
+# Exibe uma mensagem na tela
+print("Olá, Samuel! Bem-vindo à Engenharia de Dados!")
+
+#Variáveis: guardam valores na memória
+nome = "Samuel"
+idade = 22
+sonho = "se tornar um grande Engenheiro de Dados"
+
+print("Meu nome é", nome)
+print("Tenho", idade, "anos e meu sonho é", sonho)

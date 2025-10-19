5bc39e3297aa67006dd0e5d0e939c6e1294680de
Samuel Luciano <samuel.adm0017@gmail.com>
2025-10-19 16:13:15 -0100

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,8 @@
+#Desafio 1 - Entrada de dados e variáveis
+
+# Pede o nome e a idade do usuário
+nome = input("Qual é o seu nome")
+idade = input("Quantos anos você tem? ")
+
+# Mostra uma mensagem personalizada 
+print("Olá", nome + "!", "Você tem", idade, "anos e já está treinando para ser Engenheiro de Dados!")

e9eac9f751df79910c1d5aa327096e68982139b5
Samuel Luciano <samuel.adm0017@gmail.com>
2025-10-21 19:36:13 -0100

    Create desafio2.py

---
+++ Python/desafio2.py
@@ -0,0 +1,9 @@
+nome = str(input("Qual é o seu nome: "))
+ano_nascimento = int(input("Em que ano você nasceu: "))
+ano_atual = 2025
+resultado = ano_atual - ano_nascimento
+
+print("Olá,",nome + "!","Você tem",resultado,"e nasceu em",ano_nascimento)
+
+# O desafio foi pedir o nome do usuário e ano de nascimento, ao final exibe sua idade e ano em que nasceu. 
+
